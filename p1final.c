e0b34daeae3a494c8e553e445ff7d7cd9fab3f3b
sumeet siranje <94678048+Sumeet-Siranje@users.noreply.github.com>
2022-02-14 06:08:28 +0000

    coddedp1final

--- p1final.c
+++ p1final.c
@@ -1,1 +1,18 @@
-fvfdg
+#include<stdio.h>
+void input(int *a,int *b){
+  printf("enter the two numbers\n");
+  scanf("%d%d",a,b);
+}
+void sum(int a,int b,int *sum){
+  *sum=a+b;
+}
+void output(int a,int b,int c){
+  printf("the sum of %d and %d is %d\n",a,b,c);
+}
+int main(){
+  int n1,n2,n3;
+  input(&n1,&n2);
+  sum(n1,n2,&n3);
+  output(n1,n2,n3);
+  return 0;
+}
