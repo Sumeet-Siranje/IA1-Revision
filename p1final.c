334a364063a1d4f42d4c02ae4c128bb9ba3cb5b9
sumeet siranje <94678048+Sumeet-Siranje@users.noreply.github.com>
2022-02-16 14:05:18 +0000

    p1final.c_revesion

--- p1final.c
+++ p1final.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 void input(int *a,int *b){
-  printf("enter  the two numbers\n");
+  printf("enter the number\n");
   scanf("%d%d",a,b);
 }
-void sum(int a,int b,int *sum){
+void add(int a,int b,int *sum){
   *sum=a+b;
 }
 void output(int a,int b,int c){
-  printf("the sum of %d and %d is %d\n",a,b,c);
+printf("sum of %d and %d is %d\n",a,b,c);
 }
 int main(){
   int n1,n2,n3;
   input(&n1,&n2);
-  sum(n1,n2,&n3);
+  add(n1,n2,&n3);
   output(n1,n2,n3);
   return 0;
 }
+
---
+++ p4final.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int input_size_array(){
+  int n;
+  printf("enter the size of array\n"):
+  scanf("%d",&a);
+  return n;
+}
+void input_array(int n,int a[n]){
+  printf("enter the numbet\n");
+  scanf("%d",a);
+}
